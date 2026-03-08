a780721ac62259ee5c135656389d9aca8ddc718f
Luis Carlos Morales C. <luisc.moralesc@uqvirtual.edu.co>
2026-03-08 01:12:39 -0500

    cambio entorno produccion

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
 
-  backendUrl: 'https://df9oxbe03x3to.cloudfront.net',
+  backendUrl: 'https://ddep8rvr4owql.cloudfront.net',
 
   //backendUrl: 'https://inmobiliariasoft3back2-0.onrender.com' // Dirección del backend para producción
   //backendUrl: 'http://localhost:8080/api/auth',
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  backendUrl: 'https://df9oxbe03x3to.cloudfront.net',
+  backendUrl: 'https://ddep8rvr4owql.cloudfront.net',
 
   //backendUrl: 'https://backend-eden.onrender.com' // Dirección del backend para desarrollo
   //backendUrl: 'http://localhost:8080/api/auth',
