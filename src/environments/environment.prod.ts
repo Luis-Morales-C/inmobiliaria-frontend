f3454f5d74fd44c81878a2a96a25790755fdcbe8
Luis Carlos Morales C. <luisc.moralesc@uqvirtual.edu.co>
2026-04-04 04:14:37 -0500

    .

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  backendUrl: 'https://dp4yh460uscou.cloudfront.net',
+  backendUrl: 'https://d3vc9f5qlx47sj.cloudfront.net',
   recaptcha: {
     siteKey: '6LfIiYEsAAAAAE_THGKYtCqfuEwglP6os8dJYt0r' // Aquí irá la llave de tu dominio real (ej: miweb.com)
   }
