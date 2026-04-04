0469c63f17b37434308dfcf5436db0be2884e4f6
Luis Carlos Morales C. <luisc.moralesc@uqvirtual.edu.co>
2026-04-04 03:22:10 -0500

    Despliegue completado

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -100,7 +100,7 @@ export const routes: Routes = [
   },
   {
     path: '**',
-    redirectTo: '/inicio',
+    redirectTo: '',
     pathMatch: 'full'
   },
 ];
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,5 @@
 export const environment = {
   production: true,
-  //backendUrl: 'https://inmobiliariasoft3back2-0.onrender.com' // Dirección del backend para producción
   backendUrl: 'https://dp4yh460uscou.cloudfront.net',
   recaptcha: {
     siteKey: 'TU_SITE_KEY_REAL' // Aquí irá la llave de tu dominio real (ej: miweb.com)
