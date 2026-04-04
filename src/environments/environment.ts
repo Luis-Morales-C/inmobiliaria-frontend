ff038547bfc892274f0a136b723e722d5ac7050f
Luis Carlos Morales C. <luisc.moralesc@uqvirtual.edu.co>
2026-04-04 02:58:11 -0500

    .

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: false,
   //backendUrl: 'https://backend-eden.onrender.com' // Dirección del backend para desarrollo
-  backendUrl: 'd8y3oxgno7nr0.cloudfront.net',
+  backendUrl: 'https://dp4yh460uscou.cloudfront.net',
   recaptcha: {
     siteKey: '6LfIiYEsAAAAAE_THGKYtCqfuEwglP6os8dJYt0r' // La que creaste para localhost
   }
