1848178113c1233bdb21da28515aa0b1924b8663
Luis Carlos Morales C. <luisc.moralesc@uqvirtual.edu.co>
2026-04-19 02:12:21 -0500

    cambio

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   //backendUrl: 'https://inmobiliariasoft3back2-0.onrender.com' // Dirección del backend para producción
-  backendUrl: 'https://d1ohhrz57h5ohv.cloudfront.net',
+  backendUrl: 'https://d3vc9f5qlx47sj.cloudfront.net',
   recaptcha: {
     siteKey: 'TU_SITE_KEY_REAL' // Aquí irá la llave de tu dominio real (ej: miweb.com)
   }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: false,
   //backendUrl: 'https://backend-eden.onrender.com' // Dirección del backend para desarrollo
-  backendUrl: 'https://d1ohhrz57h5ohv.cloudfront.net',
+  backendUrl: 'https://d3vc9f5qlx47sj.cloudfront.net',
   recaptcha: {
     siteKey: '6LfIiYEsAAAAAE_THGKYtCqfuEwglP6os8dJYt0r' // La que creaste para localhost
   }
